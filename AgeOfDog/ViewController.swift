3cbce33a017fc995de278b97b18512ef42a4070d
Singur <aleksandrasingur@MacBook-Pro-Aleksandra-3.local>
2015-08-22 10:13:17 +0300

    first commit

--- AgeOfDog/ViewController.swift
+++ AgeOfDog/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    @IBOutlet weak var enterHumanTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let stringFromTextField = enterHumanTextField.text
+        let optionalIntFromTextField = stringFromTextField.toInt()
+        let intFromOptional = optionalIntFromTextField!
+        
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(intFromOptional * 7)" + " Human Years"
+        
+        enterHumanTextField.resignFirstResponder()
+    }
 
 }
 
